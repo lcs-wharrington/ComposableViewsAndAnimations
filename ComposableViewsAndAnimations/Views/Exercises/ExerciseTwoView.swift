d9a2f4e6f43cf9cad545b106a94d017f15b980dd
William Harrington <wharrington@lcs.on.ca>
2022-04-21 09:44:28 -0400

    Setup exercise 2

--- ComposableViewsAndAnimations/Views/Exercises/ExerciseTwoView.swift
+++ ComposableViewsAndAnimations/Views/Exercises/ExerciseTwoView.swift
@@ -14,6 +14,15 @@ struct ExerciseTwoView: View {
     
     // Controls whether this view is showing or not
     @Binding var showThisView: Bool
+    
+    // Controls the size of the circle
+    @State private var scaleFactor: CGFloat = 1.0
+    
+    // Controls the size of the circle
+    @State private var offset: CGFloat = -200.0
+    
+    // Controls the hue of the circle
+    @State private var hue: Color = .red
         
     // Whether to apply the animation
     @State private var useAnimation = false
@@ -27,9 +36,47 @@ struct ExerciseTwoView: View {
             VStack {
 
                 Circle()
-                    .frame(width: 200, height: 200)
-                    .foregroundColor(.blue)
+                //Inital cirlce size
+                    .frame(width: 300, height: 300)
                 
+                //Circle colour
+                    .foregroundColor(hue)
+                
+                //Circle size
+                    .scaleEffect(scaleFactor)
+                
+                //Circles position
+                    .offset(x: 0, y: offset)
+                
+                //Circle shrinking animation
+                    .animation(.default)
+                
+                //Iteractive Circle
+                    .onTapGesture {
+                        //Bool statment dependent on cirlce value
+                        if scaleFactor > 0.2 {
+                            
+                            // Reduce the size of the circle by a tenth
+                            scaleFactor -= 0.1
+                            
+                            //Animate positon change
+                            withAnimation(.default) {
+                                
+                                //Change position
+                                offset += 50
+                            }
+                            
+                        } else {
+                            
+                            // Make sure the button doesn't entirely disappear
+                            scaleFactor = 1
+                            
+                            // Change the color of the view to a random hue
+                            hue = Color(hue: Double.random(in: 1...360) / 360.0,
+                                        saturation: 0.8,
+                                        brightness: 0.8)
+                        }
+                    }
             }
             .navigationTitle("Exercise 2")
             .toolbar {
