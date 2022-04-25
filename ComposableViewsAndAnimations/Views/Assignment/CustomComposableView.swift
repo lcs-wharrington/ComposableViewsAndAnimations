4fef239a8e50de9ef34b5da5b7e46c821914d811
William Harrington <wharrington@lcs.on.ca>
2022-04-25 15:39:47 -0400

    First version of thumbs up complete

--- ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
+++ ComposableViewsAndAnimations/Views/Assignment/CustomComposableView.swift
@@ -7,9 +7,50 @@
 
 import SwiftUI
 
+
 struct CustomComposableView: View {
+    
+    // MARK: Stored properties
+
+    // For driving animation to reveal rectangle with progress meter fill
+        @State private var progressMeterOffset = CGSize.zero
+
+    // MARK: Computed properties
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            ZStack {
+                
+                // Stationary full rectagle
+                Image(systemName: "hand.thumbsup.fill")
+                    .frame(alignment: .center)
+                    .font(.largeTitle)
+                    .foregroundColor(.blue)
+                
+                ZStack {
+                    // White full thumbs up Will slide up
+                    Image(systemName: "hand.thumbsup.fill")
+                        .frame(alignment: .center)
+                        .font(.largeTitle)
+                        .foregroundColor(Color.primary)
+                        .colorInvert()
+
+                    // Empty thumbs up
+                    Image(systemName: "hand.thumbsup")
+                        .frame(alignment: .center)
+                        .font(.largeTitle)
+                        .foregroundColor(.blue)
+                }
+                .offset(progressMeterOffset)
+                .onTapGesture(perform: {
+                    withAnimation(Animation.easeIn(duration: 1.0)) {
+                        // Offset is moves the opaque rectangle up
+                        progressMeterOffset = CGSize(width: 0, height: -1 * (548 - 44))
+                    }
+                })
+            }
+        }
+        
     }
 }
 
